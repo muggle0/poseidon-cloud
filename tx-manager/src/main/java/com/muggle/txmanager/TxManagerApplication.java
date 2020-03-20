9b813f70c2d0f3298efcf13f71bd61aae7ad1655
muggle <isocket@outlook.com>
2020-03-20 11:30:30 +0800

    重启项目 :fire: :fire: :fire: :fire: :fire: :fire:

---
+++ tx-manager/src/main/java/com/muggle/txmanager/TxManagerApplication.java
@@ -0,0 +1,15 @@
+package com.muggle.txmanager;
+
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableTransactionManagerServer
+public class TxManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxManagerApplication.class, args);
+    }
+
+}
