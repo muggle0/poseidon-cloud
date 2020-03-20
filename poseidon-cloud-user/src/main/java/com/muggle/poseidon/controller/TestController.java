19566e532615c550521e5cac39afab0049ced0fc
muggle <isocket@outlook.com>
2020-03-20 12:06:57 +0800

    pom 文件结构调整

--- poseidon-cloud-user/src/main/java/com/muggle/poseidon/controller/TestController.java
+++ poseidon-cloud-user/src/main/java/com/muggle/poseidon/controller/TestController.java
@@ -1,20 +1,14 @@
 package com.muggle.poseidon.controller;
 
-import com.muggle.code.SimpleCodeTemplate;
-import com.muggle.code.TableMessage;
 import com.muggle.poseidon.base.ResultBean;
-import com.muggle.poseidon.feign.DocumentSeataFeign;
 import com.muggle.poseidon.service.IUserInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 import static com.muggle.poseidon.base.ResultBean.success;
 
